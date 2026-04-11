fcad60260b85b40604cea7cd22ffac1406b52f90
cde-ksh <itsmekshka@gmail.com>
2026-04-11 21:07:45 +0530

    making a change for imports in database.py

--- app/database.py
+++ app/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 from urllib.parse import quote_plus
 
 # encoding password
